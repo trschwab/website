38d7afcaf9af6457e77350c628571d845b213f0b
trschwab <trschwab7@gmail.com>
2024-05-01 11:02:54 -0400

    New Photos! I think this might be V1...

--- react_tests/my-react-app/src/pages/MusicPage.js
+++ react_tests/my-react-app/src/pages/MusicPage.js
@@ -55,6 +55,9 @@ const MusicPage = () => {
         <h4>Vinyl DJ Demos</h4>
         2023 & 2024 Vinyl DJ Set Demos, <Link to={`/vinyl`}>accessible here</Link>
         {'\n'}{'\n'} */}
+        {/* <h4>Burnt EP</h4>
+        2024 EP titled <a href="https://open.spotify.com/album/6H8F4obcl91gxBawGj7U6n?si=3aUUxat_R_uLiLLiAyaiOA" target="_blank" rel="noopener noreferrer">Burnt</a>
+        {'\n'}{'\n'} */}
         <h4>Live Guitar</h4>
         2023 performance at The Tank for one-act plays written by Nicole Frances Goth and Pat D Robinson
         {'\n'}{'\n'}
@@ -65,7 +68,7 @@ const MusicPage = () => {
         2021 Music for Gray Deuber's film titled <a href="https://www.imdb.com/title/tt14056848/" target="_blank" rel="noopener noreferrer">Sundry</a>
         {'\n'}{'\n'}
         <h4>Disco Didn't Die EP</h4>
-        2020 EP Titled <a href="https://open.spotify.com/album/6H8F4obcl91gxBawGj7U6n?si=3aUUxat_R_uLiLLiAyaiOA" target="_blank" rel="noopener noreferrer">Disco Didn't Die</a>
+        2020 EP titled <a href="https://open.spotify.com/album/6H8F4obcl91gxBawGj7U6n?si=3aUUxat_R_uLiLLiAyaiOA" target="_blank" rel="noopener noreferrer">Disco Didn't Die</a>
         {'\n'}{'\n'}
         <h4>The finest hour in an infinite journey</h4>
         2019 single for Yuan Gao's film titled <a href="https://youtu.be/p09OnCrNTow?t=746" target="_blank" rel="noopener noreferrer">The finest hour in an infinite journey</a>
