547f4a962f4584d89fb320e2da168d52a954a955
trschwab <trschwab7@gmail.com>
2024-05-04 11:01:56 -0500

    New photos for music page?

--- react_tests/my-react-app/src/pages/MusicPage.js
+++ react_tests/my-react-app/src/pages/MusicPage.js
@@ -7,21 +7,28 @@ const photos = [
 
 
   { src: "/media/IMG_9431.PNG", width: 1500, height: 2000 },
+  
   { src: "/media/6A4BFD19-2296-4B1F-90CC-616D37E71093.JPG", width: 2048, height: 1360 },
+  { src: "/media/Burnt-cover-front.jpeg", width: 3200, height: 3200 },
   { src: "/media/studio.png", width: 1000, height: 1000 },
 
+
+  { src: "/media/000307350028.jpg", width: 1800, height: 1200 },
+  { src: "/media/000307350033.jpg", width: 1200, height: 1800 },
+  { src: "/media/000307360023.jpg", width: 1800, height: 1200 },
+  { src: "/media/Earthquake.jpg", width: 1000, height: 1000 },
   { src: "/media/guitar_photo_recolored.jpeg", width: 1500, height: 1000 },
   { src: "/media/D8683A6D-8878-4AC5-A3C3-793E863E5FC4.JPG", width: 2048, height: 1360 },
 
   // { src: "/media/371DEE08-7EE6-40AA-B12A-72FA8D88D54F.JPG", width: 2048, height: 1360 },
+  { src: "/media/EP - Disco Didn't Die.jpeg", width: 1000, height: 1000 },
 
-  { src: "/media/Earthquake.jpg", width: 1000, height: 1000 },
   { src: "/media/6B353884-02D7-4C1B-BCE8-13DAA880C0FB.JPG", width: 2048, height: 1360 },
   // { src: "/media/65330005.JPG", width: 3500, height: 2400 },
   // { src: "/media/dj_pic_3.png", width: 1500, height: 2000 },
   { src: "/media/dj_pic_2.JPG", width: 1500, height: 2000 },
 
-  { src: "/media/EP - Disco Didn't Die.jpeg", width: 1000, height: 1000 },
+
   { src: "/media/NicolePlay_v2_Selects_Instagram.jpg", width: 1500, height: 2000 },
   { src: "/media/sundry.png", width: 2250, height: 1000 },
   { src: "/media/visual_disturbances_2.png", width: 1500, height: 1000 },
@@ -55,9 +62,11 @@ const MusicPage = () => {
         <h4>Vinyl DJ Demos</h4>
         2023 & 2024 Vinyl DJ Set Demos, <Link to={`/vinyl`}>accessible here</Link>
         {'\n'}{'\n'} */}
-        {/* <h4>Burnt EP</h4>
-        2024 EP titled <a href="https://open.spotify.com/album/6H8F4obcl91gxBawGj7U6n?si=3aUUxat_R_uLiLLiAyaiOA" target="_blank" rel="noopener noreferrer">Burnt</a>
-        {'\n'}{'\n'} */}
+        <h4>Burnt EP</h4>
+        2024 EP titled Burnt
+        {'\n'}
+        With Vinyl & Digital release
+        {'\n'}{'\n'}
         <h4>Live Guitar</h4>
         2023 performance at The Tank for one-act plays written by Nicole Frances Goth and Pat D Robinson
         {'\n'}{'\n'}
@@ -65,16 +74,16 @@ const MusicPage = () => {
         2021 single titled <a href="https://open.spotify.com/album/4K066FJKZ8mbmAyVF4ZBq4?si=YHy0KE72Txu8ydCd6Zue8Q" target="_blank" rel="noopener noreferrer">Earthquake</a>
         {'\n'}{'\n'}
         <h4>Sundry</h4>
-        2021 Music for Gray Deuber's film titled <a href="https://www.imdb.com/title/tt14056848/" target="_blank" rel="noopener noreferrer">Sundry</a>
+        2021 Music for Gray Deuber's short film titled <a href="https://www.imdb.com/title/tt14056848/" target="_blank" rel="noopener noreferrer">Sundry</a>
         {'\n'}{'\n'}
         <h4>Disco Didn't Die EP</h4>
         2020 EP titled <a href="https://open.spotify.com/album/6H8F4obcl91gxBawGj7U6n?si=3aUUxat_R_uLiLLiAyaiOA" target="_blank" rel="noopener noreferrer">Disco Didn't Die</a>
         {'\n'}{'\n'}
         <h4>The finest hour in an infinite journey</h4>
-        2019 single for Yuan Gao's film titled <a href="https://youtu.be/p09OnCrNTow?t=746" target="_blank" rel="noopener noreferrer">The finest hour in an infinite journey</a>
+        2019 single for Yuan Gao's short film titled <a href="https://youtu.be/p09OnCrNTow?t=746" target="_blank" rel="noopener noreferrer">The finest hour in an infinite journey</a>
         {'\n'}{'\n'}
         <h4>Visual Disturbances OST</h4>
-        2017 score for Eric Faden's film titled <a href="https://vimeo.com/302106011" target="_blank" rel="noopener noreferrer">Visual Disturbances</a>
+        2017 score for Eric Faden's feature film titled <a href="https://vimeo.com/302106011" target="_blank" rel="noopener noreferrer">Visual Disturbances</a>
         {'\n'}{'\n'}
         <h4>NYU Steinhardt</h4>
         2016 performance at NYU Steinhardt's Interactive Performance Art Concert Series.
