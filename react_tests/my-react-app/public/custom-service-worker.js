7e101772d310165ef963150910ccd31f7f147243
trschwab <trschwab7@gmail.com>
2024-06-19 01:31:03 -0400

    Not working, will revisit cache system later

--- react_tests/my-react-app/public/custom-service-worker.js
+++ react_tests/my-react-app/public/custom-service-worker.js
@@ -4,6 +4,7 @@ self.addEventListener('install', (event) => {
         return cache.addAll([
           '/photography/',
           '/media/',
+          '/blog_media/',
           // Add other assets you want to cache
         ]);
       })
