6fd878c43d0b01b7989c890ba970cc25ac9e3375
trschwab <trschwab7@gmail.com>
2024-08-08 15:31:38 -0400

    Address xml for site map and include meta data

--- react_tests/my-react-app/src/pages/HomePage.js
+++ react_tests/my-react-app/src/pages/HomePage.js
@@ -1,6 +1,7 @@
 // src/pages/HomePage.js
 import React from 'react';
 import '../styles.css';
+import { Helmet } from 'react-helmet';
 
 const HomePage = () => {
     const centerStyle = {
@@ -9,7 +10,10 @@ const HomePage = () => {
 
   return (
 	<div className="wrapper">
-
+      <Helmet>
+        <title>Home Page</title>
+        <meta name="description" content="Troy Schwab's website including details and content regarding work, music, and photography." />
+      </Helmet>
       {/* <img src="/welcome.jpg" alt="Welcome Image" /> */}
     <p style={centerStyle}>
 	<img src="/welcome.jpg" alt="photo that says welcome" class="welcome_image" />
